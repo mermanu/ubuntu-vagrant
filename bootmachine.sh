11c30a44741275550f0b415bef5e3368ea161fc9
Manuel Mérida <manuelmerida@MacBook-Pro-de-Manuel.local>
2015-10-31 09:03:53 +0100

    commit target

---
+++ bootmachine.sh
@@ -0,0 +1,19 @@
+# Install docker
+apt-get update -qq
+apt-get install -y docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+
+# Install java
+apt-get install -y openjdk-7-jdk git-core subversion maven
+
+# Install jenkins
+useradd jenkins -s /bin/bash -m -G docker
+passwd -l jenkins
+cd /home/jenkins
+sudo -Hu jenkins git config --global user.name Jenkins
+sudo -Hu jenkins git config --global user.email jenkins@$(hostname)
+wget -q -O - http://mirrors.jenkins-ci.org/war-stable/latest/jenkins.war > /home/jenkins/jenkins.war
+
+# Start jenkins
+sudo -Hu jenkins java -jar /home/jenkins/jenkins.war -Djava.awt.headless=true --httpPort=9080 &
